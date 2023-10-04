974ef16c62786e03a29692152d9d256fa39547d4
Teolyne <127517956+Teolyne@users.noreply.github.com>
2023-10-04 22:38:18 +0800

    Add files via upload

---
+++ Posttest 3/script-about-me.js
@@ -0,0 +1,29 @@
+function toggleTheme() {
+    const bodyElem = document.body;
+
+    if (bodyElem.classList.contains('light')) {
+        bodyElem.classList.remove('light');
+        document.querySelector('.theme-toggle').innerText = 'Light Mode';
+    } else {
+        bodyElem.classList.add('light');
+        document.querySelector('.theme-toggle').innerText = 'Dark Mode';
+    }
+}
+
+const gweh = document.getElementById('nama')
+gweh.style.color = 'lightblue'
+
+var move = document.querySelector('.gerak'); // Menggunakan [0] untuk mengakses elemen pertama dengan class 'gerak'
+var posisi = 0;
+
+function moveText() {
+    posisi += 10;
+    move.style.left = posisi + 'px';
+
+    if (posisi >= window.innerWidth) {
+        posisi = -move.clientWidth;
+    }
+    setTimeout(moveText,10);
+}
+
+moveText(); // Memanggil fungsi untuk memulai animasi teks
